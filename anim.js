a7effe502daad1bd4fbb710debfe12f79368b473
Alejandro-GyA <130198774+Alejandro-GyA@users.noreply.github.com>
2025-09-21 13:22:52 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -28,14 +28,14 @@ var lyricsData = [
 
 // Animar las letras
 function updateLyrics() {
-  var time = Math.floor(audio.currentTime);
+  var time = audio.currentTime;
   var currentLine = lyricsData.find(
     (line) => time >= line.time && time < line.time + 6
   );
 
   if (currentLine) {
     // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var fadeInDuration = 1.5; // Duración del efecto de aparición en segundos
     var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
 
     // Aplica el efecto de aparición
