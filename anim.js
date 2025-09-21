5b2f3dbc571c02c43d2fd1fae8986c60f67ac612
Alejandro-GyA <130198774+Alejandro-GyA@users.noreply.github.com>
2025-09-21 18:44:13 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -4,9 +4,9 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "Started out on a one-way train", time: 8 },
-  { text: "Always knew where I was gonna go next", time: 10 },
-  { text: "Didn't know until I saw your face", time: 12 },
+  { text: "Started out on a one-way train", time: 11 },
+  { text: "Always knew where I was gonna go next", time: 14 },
+  { text: "Didn't know until I saw your face", time: 16 },
   { text: "I was missing out on every moment", time: 20 },
   { text: "You'll be one and, baby, I'll be two", time: 22 },
   { text: "Would you mind it if I said I'm into you", time: 25 },
