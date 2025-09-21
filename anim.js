2a5a3082dd99004c6ffc260609315a7c1eb12a7f
Alejandro-GyA <130198774+Alejandro-GyA@users.noreply.github.com>
2025-09-21 18:09:23 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -4,26 +4,26 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "Started out on a one-way train", time: 1 },
-  { text: "Always knew where I was gonna go next", time: 1 },
-  { text: "Didn't know until I saw your face", time: 1 },
-  { text: "I was missing out on every moment", time: 10 },
-  { text: "You'll be one and, baby, I'll be two", time: 13 },
-  { text: "Would you mind it if I said I'm into you", time: 14 },
-  { text: "So if it's real, then darling let me know", time: 38 },
-  { text: "I wouldn't mind if you steal the show", time: 44 },
+  { text: "Started out on a one-way train", time: 12 }, // [00:00:12]
+  { text: "Always knew where I was gonna go next", time: 15 }, // [00:00:15]
+  { text: "Didn't know until I saw your face", time: 17 }, // [00:00:17]
+  { text: "I was missing out on every moment", time: 20 }, // [00:00:20]
+  { text: "You'll be one and, baby, I'll be two", time: 22 }, // [00:00:22]
+  { text: "Would you mind it if I said I'm into you", time: 25 }, // [00:00:25]
+  { text: "So if it's real, then darling let me know", time: 30 }, // [00:00:30]
+  { text: "I wouldn't mind if you steal the show", time: 38 }, // [00:00:38] (La letra de "I wouldn't mind" empieza en [00:00:41] y "if you steal the show" en [00:00:45])
 
-  { text: "", time: 50 }, // pausa
+  { text: "", time: 48 }, // Pausa más larga antes del siguiente verso
 
-  { text: "You and I, we go together", time: 58 },
-  { text: "You're the sky, I'll be the weather", time: 64 },
-  { text: "A pretty thing, a sun and rain, oh no", time: 70 },
-  { text: "Summer night, perfect occasion", time: 78 },
-  { text: "When I'm by, you know I'll be waiting for you", time: 85 },
-  { text: "Oh, for you", time: 91 },
+  { text: "You and I, we go together", time: 57 }, // [00:00:57]
+  { text: "You're the sky, I'll be the weather", time: 63 }, // [00:01:03]
+  { text: "A pretty thing, a sun and rain, oh no", time: 70 }, // [00:01:10]
+  { text: "Summer night, perfect occasion", time: 78 }, // [00:01:20]
+  { text: "When I'm by, you know I'll be waiting for you", time: 83 }, // [00:01:24]
+  { text: "Oh, for you", time: 89 }, // [00:01:27]
   
-  { text: "So if it's real, then darling let me know", time: 98 },
-  { text: "I wouldn't mind if you steal the show", time: 104 },
+  { text: "So if it's real, then darling let me know", time: 96 }, // [00:01:29]
+  { text: "I wouldn't mind if you steal the show", time: 104 }, // [00:01:40]
 ];
 
 // Animar las letras
